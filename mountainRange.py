04fb0bf53014bc978e59502d7961b2d88488bfd0
mrngomez <mgandroid@gmail.com>
2020-01-30 22:08:11 +0100

    random range, make it continious

--- mountainRange.py
+++ mountainRange.py
@@ -1,15 +1,28 @@
 
 import random
+import matplotlib.pyplot as plt
 
+class Mountain:
 
-def Mountain:
-
-	def __init__(heigth=100,width=200):
+	def __init__(self, heigth=100,width=200):
 
 		self.heigth = heigth #Dimensions of the mountain range
 		self.width  = width
-		self.range = {}
+		self.range = []
 
-		for _ in range(self.width):
+		for _ in range(self.width): # Generating the mountain range randomly...
+			self.range.append(random.uniform(0, self.heigth))
 
-			range.append()
+
+def main():
+
+	m = Mountain()
+	
+	plt.plot(m.range, label='Mountain range')
+	plt.xlabel('x')
+	plt.ylabel('y')
+	plt.legend()
+	plt.show()
+
+if __name__ == "__main__":
+    main()
