ec0ce88478acfa312042aad91c310fb7512085a8
mrngomez <mgandroid@gmail.com>
2020-01-30 21:48:08 +0100

    already three modules

---
+++ mountainRange.py
@@ -0,0 +1,15 @@
+
+import random
+
+
+def Mountain:
+
+	def __init__(heigth=100,width=200):
+
+		self.heigth = heigth #Dimensions of the mountain range
+		self.width  = width
+		self.range = {}
+
+		for _ in range(self.width):
+
+			range.append()
---
+++ simulatedAnnealing.py
@@ -0,0 +1,6 @@
+
+import sys
+
+def simmutaledAnnealing():
+
+	
